a99547b2af36177aebc4907cfa449d803e7270ef
Phạm Đức Toàn <32049220+1412557@users.noreply.github.com>
2017-10-23 22:17:57 +0700

    fix chinh ta

--- namemyapp/db/migrate/20171022080455_create_conversations.rb
+++ namemyapp/db/migrate/20171022080455_create_conversations.rb
@@ -7,7 +7,7 @@ class CreateConversations < ActiveRecord::Migration[5.1]
       t.timestamps
     end
 
-    add_index: conversations, :sender_id
-    add_index: conversations, :recipient_id
+    add_index :conversations, :sender_id
+    add_index :conversations, :recipient_id
   end
 end
